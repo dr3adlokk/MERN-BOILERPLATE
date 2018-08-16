b9ae209aa97d227c1cf08c4c2281e7a842739528
John Chambers <develop@Johns-MacBook-Pro.local>
2018-08-16 07:24:26 -0700

    mongo

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose')
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -10,10 +20,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+app.listen(PORT, () => {
+  console.log(`🌎 ==> App served to port ${PORT}!`);
 });
